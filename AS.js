f3d5aa3b7f96617b8130c87ce525cf40f2c2bb6b
BlueWorldInc <keamybakunin@gmail.com>
2019-11-09 20:56:11 +0100

    [add] making a grid complete

--- AS.js
+++ AS.js
@@ -1,20 +1,27 @@
-var c = document.getElementById("myCanvas");
+var c = document.getElementById("CanvasAS");
 var ctx = c.getContext("2d");
 // drawLine();
-logTimer()
+gridDrawer();
 
-function drawLine(x) {
-    ctx.moveTo(0, 0);
-    ctx.lineTo((x * 10), 200);
+function drawLineVertical(x) {
+    ctx.beginPath();
+    ctx.moveTo((x * 20), 0);
+    ctx.lineTo((x * 20), 200);
     ctx.stroke();
 }
 
-async function logTimer() {
-    var x = 0;
-    for (var i = 0; i < 400; i++) {
-        drawLine(x);
-        console.log(++x);
-        await sleep(10);
+function drawLineHorizontal(y) {
+    ctx.beginPath();
+    ctx.moveTo(0, (y * 20));
+    ctx.lineTo(400, (y * 20));
+    ctx.stroke();
+}
+
+async function gridDrawer() {
+    for (var i = 0; i < 20; i++) {
+        drawLineVertical(i);
+        drawLineHorizontal(i);
+        await sleep(100);
     }
 }
 
