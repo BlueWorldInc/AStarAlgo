377714d34f95a15522ddc346a6b30ccae2ffa502
BlueWorldInc <keamybakunin@gmail.com>
2019-11-16 19:45:00 +0100

    [add] adding surface type

--- AS.js
+++ AS.js
@@ -21,6 +21,10 @@ var directions = {
     }
 }
 
+// var surfaces = [[0,0,"normal"],[0,2,"normal"]];
+
+var surfaceType = "normal";
+
 gridDrawer();
 getDistanceX();
 
@@ -47,7 +51,7 @@ async function gridDrawer() {
 }
 
 function colorSquare(x, y, color) {
-    ctx.fillStyle = color;
+    ctx.fillStyle = colorOfSurface();
     ctx.fillRect((x * 20), (y * 20), 20, 20);
     ctx.stroke();
 }
@@ -56,8 +60,8 @@ function colorSelectedSquareOnClick() {
     var rect = c.getBoundingClientRect();
     var x = event.clientX - Math.round(rect.left);
     var y = event.clientY - Math.round(rect.top);
-    if (x >= 400) { x = 399;}
-    if (y >= 200) { y = 199;}
+    if (x >= 400) { x = 399; }
+    if (y >= 200) { y = 199; }
     //clear canvas
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, 400, 200);
@@ -84,7 +88,7 @@ async function colorIntermediary() {
     //Intermediary Square
     var IS = {
         x: 0,
-        y: 0        
+        y: 0
     }
     var sl = squares.lastSquare;
     var sc = squares.currentSquare;
@@ -105,91 +109,91 @@ async function colorIntermediary() {
             colorSquare(sl.x, sl.y - (Math.sign(diff) * i), "red");
         }
     } else
-     if (sl.x !== sc.x && sl.y !== sc.y) {
+        if (sl.x !== sc.x && sl.y !== sc.y) {
 
-        diagonalNumber = Math.min(Math.abs(xOffset), Math.abs(yOffset));
-        directLineNumber = Math.abs(Math.abs(xOffset) - Math.abs(yOffset));
-
-        if (xOffset > 0 && yOffset < 0) {
-            dd.d0 = -1;
-            dd.d1 = 1;
-        }
-        if (xOffset < 0 && yOffset < 0) {
-            dd.d0 = -1;
-            dd.d1 = -1;
-        }
-        if (xOffset > 0 && yOffset > 0) {
-            dd.d0 = 1;
-            dd.d1 = 1;
-        }
-        if (xOffset < 0 && yOffset > 0) {
-            dd.d0 = 1;
-            dd.d1 = -1;
-        }
-
-        if (directLineNumber > 0) {
-            if (xOffset < 0 && yOffset > 0) {
-                if (xOffset + yOffset > 0) {
-                    dld.y = -1;
-                    dld.x = 0;
-                } else {
-                    dld.x = 1;
-                    dld.y = 0;
-                }
-            }
-
-            if (xOffset < 0 && yOffset < 0) {
-                if (xOffset - yOffset > 0) {
-                    dld.y = 1;
-                    dld.x = 0;
-                } else {
-                    dld.x = 1;
-                    dld.y = 0;
-                }
-            }
-
-            if (xOffset > 0 && yOffset > 0) {
-                if (xOffset - yOffset < 0) {
-                    dld.y = -1;
-                    dld.x = 0;
-                } else {
-                    dld.x = -1;
-                    dld.y = 0;
-                }
-            }
+            diagonalNumber = Math.min(Math.abs(xOffset), Math.abs(yOffset));
+            directLineNumber = Math.abs(Math.abs(xOffset) - Math.abs(yOffset));
 
             if (xOffset > 0 && yOffset < 0) {
-                if (xOffset + yOffset < 0) {
-                    dld.y = 1;
-                    dld.x = 0;
-                } else {
-                    dld.x = -1;
-                    dld.y = 0;
+                dd.d0 = -1;
+                dd.d1 = 1;
+            }
+            if (xOffset < 0 && yOffset < 0) {
+                dd.d0 = -1;
+                dd.d1 = -1;
+            }
+            if (xOffset > 0 && yOffset > 0) {
+                dd.d0 = 1;
+                dd.d1 = 1;
+            }
+            if (xOffset < 0 && yOffset > 0) {
+                dd.d0 = 1;
+                dd.d1 = -1;
+            }
+
+            if (directLineNumber > 0) {
+                if (xOffset < 0 && yOffset > 0) {
+                    if (xOffset + yOffset > 0) {
+                        dld.y = -1;
+                        dld.x = 0;
+                    } else {
+                        dld.x = 1;
+                        dld.y = 0;
+                    }
+                }
+
+                if (xOffset < 0 && yOffset < 0) {
+                    if (xOffset - yOffset > 0) {
+                        dld.y = 1;
+                        dld.x = 0;
+                    } else {
+                        dld.x = 1;
+                        dld.y = 0;
+                    }
+                }
+
+                if (xOffset > 0 && yOffset > 0) {
+                    if (xOffset - yOffset < 0) {
+                        dld.y = -1;
+                        dld.x = 0;
+                    } else {
+                        dld.x = -1;
+                        dld.y = 0;
+                    }
+                }
+
+                if (xOffset > 0 && yOffset < 0) {
+                    if (xOffset + yOffset < 0) {
+                        dld.y = 1;
+                        dld.x = 0;
+                    } else {
+                        dld.x = -1;
+                        dld.y = 0;
+                    }
                 }
             }
-        }
 
-        IS.x = sl.x;
-        IS.y = sl.y;
+            IS.x = sl.x;
+            IS.y = sl.y;
 
 
-        for (var i = 1; i <= diagonalNumber; i++) {
-            if (i == diagonalNumber && directLineNumber == 0) {
-                break;
+            for (var i = 1; i <= diagonalNumber; i++) {
+                if (i == diagonalNumber && directLineNumber == 0) {
+                    break;
+                }
+                await sleep(50);
+                IS.x -= dd.d1;
+                IS.y -= dd.d0;
+                colorSquare(IS.x, IS.y, "red");
             }
-            await sleep(50);
-            IS.x -= dd.d1;
-            IS.y -= dd.d0;
-            colorSquare(IS.x, IS.y, "red");
+
+            for (var i = 1; i < directLineNumber; i++) {
+                await sleep(50);
+                IS.x += dld.x;
+                IS.y += dld.y;
+                colorSquare(IS.x, IS.y, "red");
+            }
         }
-        
-        for (var i = 1; i < directLineNumber; i++) {
-            await sleep(50);
-            IS.x += dld.x;
-            IS.y += dld.y;
-            colorSquare(IS.x, IS.y, "red");
-        }
-    }
 }
 
 function sleep(ms) {
@@ -215,3 +219,20 @@ function getDistanceX() {
     }
     document.getElementById("dist").innerHTML = d;
 }
+
+function changeSurface(surfaceType) {
+    this.surfaceType = surfaceType;
+}
+
+function colorOfSurface() {
+    if (this.surfaceType == "normal") {
+        return "black";
+    } else if (this.surfaceType == "intermediary") {
+    return "red";
+    } else if (this.surfaceType == "water") {
+    return "blue";
+    } else if (this.surfaceType == "obstacle") {
+    return "grey";
+    }
+}
+
